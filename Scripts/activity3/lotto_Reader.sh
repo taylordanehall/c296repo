80bd18685d7a78c7eb8ef452af2cb1247b205ae9
EC2 Default User <ec2-user@ip-172-31-31-35.us-east-2.compute.internal>
2022-10-20 19:54:31 +0000

    Updated grep regex

--- Scripts/activity3/lotto_Reader.sh
+++ Scripts/activity3/lotto_Reader.sh
@@ -9,9 +9,9 @@ while [ $CHECK -eq 1 ];do
 	echo "Enter your Lottery numbers, seperated by spaces (eg. 2 34 12 45 21):"
 	read USER_IN
 	for i in $USER_IN;do
-		echo $USER_IN | grep '[a-zA-Z]'
+		echo $USER_IN | grep -q '[^0-9]'
 		G=$?
-		if [ $i -lt 51 ] && [ $i -gt 0 ] && [ $G -eq 1  ];then
+		if [ $i -lt 51 ] && [ $i -gt 0 ] && [ $G -eq 0  ];then
 			CHECK=0
 		else
 			echo "Make sure you enter everything correctly!"
